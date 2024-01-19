671b5b7079ceb9c9eccdd4e1a4e184a7b85f43c1
amanecopse <amanecopse@gmail.com>
2024-01-19 13:41:12 +0900

    feat: 기본 rest 서버 셋팅

---
+++ src/main/java/org/example/board/controller/BoardController.java
@@ -0,0 +1,15 @@
+package org.example.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("")
+    public String exampleMain() {
+        return "boardList";
+    }
+}
---
+++ src/main/java/org/example/home/controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.example.home.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("")
+    public String exampleMain() {
+        return "home";
+    }
+}
