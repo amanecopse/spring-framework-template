061427c3576d539f34ed48422c7e5d3b5b109e40
amanecopse <amanecopse@gmail.com>
2024-02-26 12:44:52 +0900

    feat: 로그인 관련 에러 핸들링

--- src/main/java/org/example/domain/member/application/AuthService.java
+++ src/main/java/org/example/domain/member/application/AuthService.java
@@ -5,7 +5,10 @@ import org.example.domain.member.dao.UserDao;
 import org.example.domain.member.dto.LoginRequest;
 import org.example.domain.member.dto.SignupRequest;
 import org.example.domain.member.dto.UserMapper;
+import org.example.domain.member.exception.LoginFailedException;
 import org.example.global.config.security.TokenProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -31,7 +34,14 @@ public class AuthService {
     public String authenticate(LoginRequest request) {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        Authentication authentication;
+        try {
+            authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new LoginFailedException("비밀번호가 일치하지 않습니다");
+        } catch (InternalAuthenticationServiceException e) {
+            throw new LoginFailedException("존재하지 않는 유저입니다");
+        }
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return tokenProvider.createToken(authentication);
     }
